a0ecdeb14ec555ecdf18f4a665e3d8da6a1b4582
kabiru <mwangikabiru21@gmail.com>
2024-08-24 17:32:51 +0300

    Added description

--- src/app/page.js
+++ src/app/page.js
@@ -3,8 +3,9 @@ import Chessboard from "@/components/Chessboard";
 export default function Home() {
   return (
     <main className="flex flex-col h-screen w-screen items-center justify-between gap-4">
-      <div className="w-full justify-center place-items-center font-mono p-4 border-b-2 border-white-500">
+      <div className="flex flex-col gap-2 w-full justify-center place-items-center font-mono p-4 border-b-2 border-white-500">
         <p className="font-mono font-bold text-2xl text-center">drawless</p>
+        <p className="font-mono font-bold text-lg text-center">play and insert random pieces when you get in a fix - enjoy!</p>
       </div>
 
       <div className="w-full h-full flex flex-col gap-4 justify-center place-items-center">
